9d4ab8d3df6a3242290c9155898adb9ecc99f79d
Om Gupta <omgupta1608@gmail.com>
2023-07-11 21:07:25 +0530

    fixed error handling in crypto_c

--- cli/crypto_c.py
+++ cli/crypto_c.py
@@ -15,18 +15,21 @@ def encrypt(key: str, source: str, encode=True):
         source += bytes([padding]) * padding
         data = IV + encryptor.encrypt(source)  
         return base64.b64encode(data).decode("latin-1") if encode else data
-    except:
-        raise
+    except Exception as e:
+        raise RuntimeError("Encrypt error: ", e)
 
 def decrypt(key: str, source, decode=True):
-    key = key.encode("UTF-8")
-    if decode:
-        source = base64.b64decode(source.encode("latin-1"))
-    key = SHA256.new(key).digest() 
-    IV = source[:AES.block_size]  
-    decryptor = AES.new(key, AES.MODE_CBC, IV)
-    data = decryptor.decrypt(source[AES.block_size:])
-    padding = data[-1]  
-    if data[-padding:] != bytes([padding]) * padding:
-        raise ValueError("Invalid padding...")
-    return data[:-padding].decode("UTF-8") 
+    try: 
+        key = key.encode("UTF-8")
+        if decode:
+            source = base64.b64decode(source.encode("latin-1"))
+        key = SHA256.new(key).digest() 
+        IV = source[:AES.block_size]  
+        decryptor = AES.new(key, AES.MODE_CBC, IV)
+        data = decryptor.decrypt(source[AES.block_size:])
+        padding = data[-1]  
+        if data[-padding:] != bytes([padding]) * padding:
+            raise ValueError("Invalid padding...")
+        return data[:-padding].decode("UTF-8")
+    except Exception as e:
+        raise RuntimeError("Decrypt error: ", e)
