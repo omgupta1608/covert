eb61e0ef6a09edf6b9bde85d15a1faf16687bf42
sahilchalia <37237782+sahilchalia@users.noreply.github.com>
2023-07-12 10:14:14 +0530

    basic home page

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
      <h2>Work in Progress</h2>
+     <Navbar />
+     <Home />
     </div>
   );
 }
